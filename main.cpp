94fd1424b1ec353264a619a38e12c746f9c89aa2
pizzaani <pizza.ani@gmx.de>
2018-05-24 15:33:46 +0000

    counter update for both edges
    Signed-off-by: pizzaani <pizza.ani@gmx.de>

--- main.cpp
+++ main.cpp
@@ -6,13 +6,15 @@
 
 #define INT_PIN  29
 
-volatile uint32_t counter = 0;
+volatile int counter = 0;
 
-void risingEdge(void)
+void bothEdge(void)
 {
-  digitalRead(INT_PIN);
-  printf("Calling %s with counter at %d\n",__func__, counter);
-  counter++;
+  //This is a workaround to clear ISR
+  int v = digitalRead(INT_PIN)*2-1;
+  
+  counter+=v;
+  printf("Calling %s with counter at %+d and read = %d\n",__func__, counter, v);
 }
 
 int main(int argc, char **argv)
@@ -28,7 +30,7 @@ int main(int argc, char **argv)
   pinMode(pin, INPUT);
   pullUpDnControl (pin, PUD_DOWN); 
 
-  if (wiringPiISR (pin, INT_EDGE_RISING, &risingEdge) != 0)
+  if (wiringPiISR (pin, INT_EDGE_BOTH, &bothEdge) != 0)
   {
     printf("wiringPi ISR failed! Exiting!\n");
     return 1;
